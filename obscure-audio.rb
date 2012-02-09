4cfdd1665bf317638cb638c4ce186c755e3f5be6
Mike Phillips <msphil@gmail.com>
2012-02-08 23:40:55 -0800

    Start implementing basic functionality

--- obscure-audio.rb
+++ obscure-audio.rb
@@ -4,6 +4,7 @@ require 'sinatra'
 require 'redis'
 
 # TODO:
+#   Break pages out into views
 #   Fancier main page
 #   Form for poll entry
 #   Poll display
@@ -12,7 +13,7 @@ require 'redis'
 
 configure do
   REDIS = Redis.new
-  REDIS.setnx 'key', 10 * 36**4
+  REDIS.setnx 'key', 10 * 36**4 # start with a0000
 end
 
 get '/' do
@@ -22,9 +23,26 @@ end
 
 get '/create' do
   # create poll with new hash here
-  #
-  # placeholder:
-  erb :index
+
+  @numquestions = params[:numquestions].to_i
+  if @numquestions < 1
+    erb :create
+  else
+    erb :create2
+  end
+end
+
+#post '/create:numquestions' do
+#  @numquestions = params[:numquestions]
+#  erb :create2
+#end
+
+post '/finalize' do
+  # storing poll goes here
+  @key = REDIS.get('key').to_i
+  @key += random 10 # new key
+  REDIS.set 'key', @key.to_s(36)
+  erb :final
 end
 
 get '/poll' do
@@ -48,6 +66,11 @@ get '/results/:poll' do
   erb :index
 end
 
+not_found do
+  @hp = request.host_with_port
+  erb :notfound
+end
+
 __END__
 @@index
 <!DOCTYPE html>
@@ -61,3 +84,61 @@ __END__
     <p><a href="results">View poll results</a></p>
   </body>
 </html>
+
+@@notfound
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Obscure Audio Internet Poll Creator</title>
+  </head>
+  <body>
+    <p>You have reached an invalid location.</p>
+    <p>Please return to <a href="http://<%= @hp %>">the index</a></p>
+  </body>
+</html>
+
+@@create
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Obscure Audio Internet Poll Creator</title>
+  </head>
+  <body>
+    <form>
+      Number of questions: 
+        <input type="text" name="numquestions">
+    </form>
+  </body>
+</html>
+
+@@create2
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Obscure Audio Internet Poll Creator</title>
+  </head>
+  <body>
+    <form>
+      <p>Poll Question:
+        <input type="text" name="question" size="40">
+      </p>
+      <% 1.upto(@numquestions) { |i|  %>
+        <p>Question <%= i %>:
+          <input type="text" name="question<%= i %>" size="40">
+        </p>
+      <% } %>
+    </form>
+  </body>
+</html>
+
+@@finalize
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Obscure Audio Internet Poll Creator</title>
+  </head>
+  <body>
+    <p>You created poll <%= @key %>, which may be accessed <a href="http://<%= @hp =>/poll/<%= @key %>">here</a>.
+  </body>
+</html>
+
